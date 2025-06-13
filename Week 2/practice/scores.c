0d2cdcd90e3fe11a9b8fd4fdce7ae6fa7b05733f
Thijs Moens <thijs.willem.moens@gmail.com>
2025-06-13 15:14:19 +0200

    Add practice week 2

---
+++ Week 2/practice/scores.c
@@ -0,0 +1,13 @@
+#include <stdio.h>;
+
+int main(void)
+{
+	int scores[3];
+
+	scores[0] = 75;
+	scores[1] = 68;
+	scores[2] = 45;
+
+	// Print average score
+	printf("Average: %f\n", (scores[0], scores[1], scores[2] / 3.0));
+}
