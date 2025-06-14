8c272e8d8d550db86ebbbaa2589a568d0d33c82b
Thijs Moens <thijs.willem.moens@gmail.com>
2025-06-14 15:42:26 +0200

    Update on scrabble

--- Week 2/problem_set_2/scrabble/scrabble.c
+++ Week 2/problem_set_2/scrabble/scrabble.c
@@ -7,6 +7,13 @@
 #include <stdio.h>
 #include <string.h>
 
+// Function decleration
+int calculateTotalScore(string word);
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+
 int main ()
 {
 
@@ -14,27 +21,49 @@ int main ()
 	string playerOneWord = get_string("Player 1: ");
 
 	// Prompt Player 2 for picking a word
-	string playerTwoWord = get_string("Player 2 ");
+	string playerTwoWord = get_string("Player 2: ");
 
-	// sum the letters for points for player 1 and 2
+	// Calculate points for player 1 and 2
+	int scorePlayerOne = calculateTotalScore(playerOneWord);
+	int scorePlayerTwo = calculateTotalScore(playerTwoWord);
 
-	// Check who has won or if it is a tie
-
-	// Print the result
-
-	printf("Player 1: %s\n", playerOneWord);
-	printf("Player 2: %s\n", playerTwoWord);
-
+	// Check who has won and print the result
+	if (scorePlayerOne < scorePlayerTwo)
+	{
+		printf("Player 2 wins!\n");
+	}
+	else if (scorePlayerOne > scorePlayerTwo)
+	{
+		printf("Player 1 wins!\n");
+	}
+	else
+	{
+		printf("Tie!\n");
+	}
 
 
 	return 0;
 
 }
 
-
-// input is Question
-
-int playerOne(int count, string name[])
+int calculateTotalScore(string word)
 {
+	// Keep track of score
+	int score = 0;
 
+ 	// Compute score for each character
+    for (int i = 0, lenght = strlen(word); i < lenght; i++)
+    {
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+    }
+
+
+	return score;
 }
