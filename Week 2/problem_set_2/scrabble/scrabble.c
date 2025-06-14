4d2d9f478e0738acfce8bd24d0a9fe1bfabb1445
Thijs Moens <thijs.willem.moens@gmail.com>
2025-06-14 14:51:20 +0200

    Create scrabble for problem set 2

---
+++ Week 2/problem_set_2/scrabble/scrabble.c
@@ -0,0 +1,40 @@
+/*
+	Created by Thijs Moens on 14 june 2025
+*/
+
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+
+	// Prompt Player 1 for picking a word
+	string playerOneWord = get_string("Player 1: ");
+
+	// Prompt Player 2 for picking a word
+	string playerTwoWord = get_string("Player 2 ");
+
+	// sum the letters for points for player 1 and 2
+
+	// Check who has won or if it is a tie
+
+	// Print the result
+
+	printf("Player 1: %s\n", playerOneWord);
+	printf("Player 2: %s\n", playerTwoWord);
+
+
+
+	return 0;
+
+}
+
+
+// input is Question
+
+int playerOne(int count, string name[])
+{
+
+}
