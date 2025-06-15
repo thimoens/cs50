5c0c209b5d0cf26e898a76b74c1e7f202f2f2984
Thijs Moens <thijs.willem.moens@gmail.com>
2025-06-15 18:04:10 +0200

    Create Caesar problem set

---
+++ Week 2/problem_set_2/caesar/caesar.c
@@ -0,0 +1,28 @@
+/*
+	Created by Thijs Moens on 15 june 2025
+
+
+	Unencrypted text is generally called plaintext. 
+	Encrypted text is generally called ciphertext. 
+	And the secret used is called a key.
+*/
+
+// Import libraries
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char argv[])
+{
+
+	// User should provide the key number in the command line
+	printf("%c", argv[]);
+
+	// The key must be an positive integer
+
+
+
+	return 0;
+}
