b1228630d6314b490000e5fd5b07f664bef57929
Thijs Moens <thijs.willem.moens@gmail.com>
2025-06-20 10:04:50 +0200

    Update caesar

--- Week 2/problem_set_2/caesar/caesar.c
+++ Week 2/problem_set_2/caesar/caesar.c
@@ -14,15 +14,47 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char **argv)
+int main(int argc, string argv[])
 {
 
-	// User should provide the key number in the command line
-	printf("%i\n", key);
+	// Check for 1 command line input
+	if (argc == 0 || argc > 1)
+	{
+		printf("Usage: ./caesar key\n");
 
-	// The key must be an positive integer
+		return 1;
+	}
+	else 
+	{
+		return 0;
+	}
+	
+
+    // Check if character in argv[1] is a digit
+	
 
 
 
-	return 0;
+    // // Make sure every character in argv[1] is a digit
+	// for (int i = 0; i < strlen(argv[1]); i++)
+	// {
+	// 	printf("%s\n", argv[1]);
+	// 	// if (isdigit(argv[i]))
+	// 	// {
+	// 	// 	printf("Your input is a digit.\n");
+	// 	// }
+	// 	// else
+	// 	// {
+	// 	// 	printf("Your input is not a digit.\n");
+	// 	// }
+	// }
+
+    // Convert argv[1] from a `string` to an `int`
+
+    // Prompt user for plaintext
+
+    // For each character in the plaintext:
+
+        // Rotate the character if it's a letter
+
 }
